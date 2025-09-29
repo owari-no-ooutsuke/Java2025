d9a7a494097a98c6402a1ee284e216c8ce5509d5
owari-no-ooutsuke <nastyavolgina158@gmail.com>
2025-09-29 19:12:33 +0300

    answer

---
+++ NamePrint.java
@@ -0,0 +1,7 @@
+public class NamePrint {
+    public static void main(String[] args) {
+        System.out.println("" + '\u0410' + '\u043D' + '\u0430' + '\u0441' +
+                '\u0442' + '\u0430' + '\u0441' + '\u0438'
+                + '\u044F'); //Вывод: Анастасия
+    }
+}
