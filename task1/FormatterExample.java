6c0f2fa299fadf69142482873e38bee79608b421
owari-no-ooutsuke <nastyavolgina158@gmail.com>
2025-11-08 18:45:48 +0300

    answer1

---
+++ task1/FormatterExample.java
@@ -0,0 +1,19 @@
+import java.util.Formatter;
+
+public class FormatterExample {
+    public static void main(String[] args) {
+        Formatter formatter = new Formatter();
+
+        int a = 2;
+        int b = 35;
+        double percent = (double)a / b * 100;
+        //Используем %d, %f, %%
+        formatter.format("Число %d составляет %f%% от числа %d", a, percent, b);
+
+        String word = "hello";
+        //Используем %n, %c
+        formatter.format("%nПервая буква слова - %c", word.charAt(0));
+
+        System.out.println(formatter);
+    }
+}
